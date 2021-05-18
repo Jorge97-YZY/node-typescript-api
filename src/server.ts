5f7d5c9801612c57d845b5c29d688e358786c455
Jorge Manuel <jorgeyeez@gmail.com>
2021-05-18 21:42:02 +0100

    chose: Configuração do Prettier

--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,26 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { Application } from 'express';
 import { ForecastController } from './controllers/forecast';
-import './util/module-alias'
+import './util/module-alias';
 
-export class SetupServer extends Server{
-    
-    constructor(private port = 3000){
-        super();
-    }
-    public init(): void{
-        this.setupExpress();
-        this.setupControllers();
-    }
-    private setupExpress(): void{
-        this.app.use(bodyParser.json());
-    }
+export class SetupServer extends Server {
+  constructor(private port = 3000) {
+    super();
+  }
+  public init(): void {
+    this.setupExpress();
+    this.setupControllers();
+  }
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-    private setupControllers(): void{
-        const foreCastController = new ForecastController();
-        this.addControllers(foreCastController); 
-    }
+  private setupControllers(): void {
+    const foreCastController = new ForecastController();
+    this.addControllers(foreCastController);
+  }
 
-    public getApp(): Application{
-        return this.app;
-    }
+  public getApp(): Application {
+    return this.app;
+  }
 }
